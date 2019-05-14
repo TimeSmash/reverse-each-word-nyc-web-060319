7266a34cd5570dbf7880756e1dc7d2e2fc2776ef
mcumm64 <mcumm64@gmail.com>
2019-05-14 23:40:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word  (sentence)
+  # sent_split = sentence.split(" ")
   reverse_sent = []
-  sentence.each do |word|
+  sentence.split(" ").each do |word|
       reverse_sent << word.reverse
   end
   reverse_sent
