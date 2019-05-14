0880e0b95c7a8b1d2c85b93544b3e7b82929e996
mcumm64 <mcumm64@gmail.com>
2019-05-14 23:36:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word  (string, sentence)
-  # sentence = sentence.split(" ")
-  # if sentence.include?(string)
   new_sent = sentence.each do |word|
     if word == string
       sentence[word] = string
     end
-  end  
-    new_sent
+  end
+  new_sent
 end
