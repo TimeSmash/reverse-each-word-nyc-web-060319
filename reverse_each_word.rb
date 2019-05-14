fa1b30af9e147dbb4d9d52d451c56d8f537b5037
mcumm64 <mcumm64@gmail.com>
2019-05-14 23:34:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
 def reverse_each_word  (string, sentence)
-  sentence = sentence.split(" ")
-  if sentence.include?(word)
-    
+  # sentence = sentence.split(" ")
+  # if sentence.include?(string)
+  new_sent = sentence.each do |word|
+  if word == string
+    sentence[word] = string
+  end
+  new_sent
+end
